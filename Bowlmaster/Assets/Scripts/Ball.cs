c8815e4dcff757ca15d719eaa38ac4d093189b1b
Wade Calvert <wcalvert88@gmail.com>
2018-12-19 14:56:29 -0500

    created a ball and got it moving and making sound

---
+++ Bowlmaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] public float launchSpeed = 200f;
+
+    [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+        Launch();
+    }
+
+    public void Launch() {
+        rigidBody.velocity = new Vector3(0,0, launchSpeed);
+        audioSource.Play();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
