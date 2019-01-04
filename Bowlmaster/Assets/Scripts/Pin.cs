e76155eee6be0f580d305420d1fcbd7050990750
Wade Calvert <wcalvert88@gmail.com>
2019-01-04 10:28:14 -0500

    created a method IsStanding to check if the pins are still standing

---
+++ Bowlmaster/Assets/Scripts/Pin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    [SerializeField] public float standingThreshold = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(name + IsStanding());
+    }
+
+    public bool IsStanding() {
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+        float tiltInX = Mathf.Abs(rotationInEuler.x);
+        float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+        if ((tiltInX < standingThreshold || (tiltInX <= 360f && tiltInX >= 360f - standingThreshold)) || (tiltInZ < standingThreshold || (tiltInZ <= 360f && tiltInZ >= 360f - standingThreshold))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
