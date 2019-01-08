9bb570a81f3a52cd96727f62afcb98e17f747d97
Wade Calvert <wcalvert88@gmail.com>
2019-01-08 15:33:30 -0500

    created test for spares and when just bowling an 8

--- Bowlmaster/Assets/Scripts/ActionMaster.cs
+++ Bowlmaster/Assets/Scripts/ActionMaster.cs
@@ -6,18 +6,37 @@ public class ActionMaster
 {
     public enum Action {Tidy, Reset, EndTurn, EndGame};
 
+    // private int[] bowls = new int[21];
+    private int bowl = 1;
+
     public Action Bowl(int pins) {
 
         if (pins < 0 || pins > 10) {
             throw new UnityException("Invalid number of pins!");
         }
 
-        // Other behaviour here
+        // Other behaviour here, e.g. last frame
+        // T01
         if (pins == 10) {
+            bowl += 2;
             return Action.EndTurn;
         }
 
-        // Other behaviour here
+        // T02
+        // If first bowl of frame return Action.Tidy
+        if (bowl % 2 != 0) { // Mid frame (or last frame)
+            if (pins > 0  && pins < 10) {
+                bowl += 1;
+                return Action.Tidy;
+            }
+        // T03
+        } else if (bowl % 2 == 0) {
+            // End of frame
+            bowl += 1;
+            return Action.EndTurn;
+        }
+        
+
         throw new UnityException("Not sure what action to return!");
     }
 }
