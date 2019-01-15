c1a0de51b23e6ac671da83c455ba25ee03a8ee6b
Wade Calvert <wcalvert88@gmail.com>
2019-01-15 10:10:00 -0500

    created ScoreMaster.cs in preparation for a complete architecture change

---
+++ Bowlmaster/Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster {
+
+    public static List<int> ScoreFrames(List<int> rolls) {
+        List<int> frameList = new List<int>();
+
+        // Your code here
+
+        return frameList;
+    }
+}
